882807a79252a57477d3d1276789b38ce306a4bd
dzzesen <kopeykindzzesen@gmail.com>
2024-12-26 12:45:15 +0300

    move treesitter settings to options file

--- configs/nvim/lua/plugins/options.lua
+++ configs/nvim/lua/plugins/options.lua
@@ -1,3 +1,4 @@
 require("plugins.cokeline.options")
 require("plugins.lualine.options")
 require("plugins.lorem.options")
+require("plugins.treesitter.options")
--- configs/nvim/lua/plugins/specs.lua
+++ configs/nvim/lua/plugins/specs.lua
@@ -76,6 +76,10 @@ local plugin_specs = {
     },
     {
         "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup(treesitter_options)
+            vim.cmd("TSUpdate")
+        end
     },
     {
         "OXY2DEV/markview.nvim",
@@ -88,28 +92,3 @@ local plugin_specs = {
 }
 
 require("lazy").setup(plugin_specs)
-require("nvim-treesitter.configs").setup({
-    ensure_installed = { 
-        "lua",
-        "vim",
-        "vimdoc",
-        "markdown",
-        "markdown_inline",
-        "html",
-        "latex",
-        "python",
-    },
-    sync_install = true,
-    auto_install = true,
-    highlight = {
-        enable = true,
-        disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end,
-        additional_vim_regex_highlighting = false,
-    },
-})
