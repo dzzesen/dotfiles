bc6acb1e366e2a4ab3675819671cb339a2fb3b5b
dzzesen <kopeykindzzesen@gmail.com>
2024-12-26 13:31:17 +0300

    simplify hop keymaps

--- configs/nvim/lua/plugins/hop/mappings.lua
+++ configs/nvim/lua/plugins/hop/mappings.lua
@@ -1,4 +1,3 @@
-vim.keymap.set("n", "f", function() require("hop").hint_words({current_line_only=true}) end)
-vim.keymap.set("n", "F", function() require("hop").hint_words() end)
-vim.keymap.set("v", "f", function() require("hop").hint_words({current_line_only=true}) end)
-vim.keymap.set("v", "F", function() require("hop").hint_words() end)
+local hop = require("hop")
+vim.keymap.set("", "f", function() hop.hint_words({current_line_only=true}) end)
+vim.keymap.set("", "F", function() hop.hint_words() end)
