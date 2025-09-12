0ed151f3968606d31550cefed8adb2b7a0d0eb4d
Dzenis Kopeikin <kopeykindzzesen@gmail.com>
2025-09-12 11:46:37 +0400

    configure lazy.nvim

--- configs/nvim/lua/config/lazy.lua
+++ configs/nvim/lua/config/lazy.lua
@@ -2,12 +2,23 @@
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    -- { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
+  defaults = { version = false },
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "catppuccin" } },
+  ui = {
+    size = { width = 0.55, height = 0.90 },
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    backdrop = 100,
+    title = "lazy.nvim",
+    title_pos = "right",
+  },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    frequency = 86400, -- check for updates every day
+  },
 })
 
--- configs/nvim/lua/config/mappings.lua
+++ configs/nvim/lua/config/mappings.lua
@@ -2,4 +2,4 @@
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = ";"
