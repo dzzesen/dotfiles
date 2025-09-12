ecf05fb765a295995b5f7c07632aad668ccc09c9
Dzenis Kopeikin <kopeykindzzesen@gmail.com>
2025-09-12 23:06:11 +0400

    rename lua_ls to lua_language_server

--- configs/nvim/lua/lsp/init.lua
+++ configs/nvim/lua/lsp/init.lua
@@ -1,5 +1,5 @@
-vim.lsp.config("lua_ls", require("lsp.lua"))
-vim.lsp.enable("lua_ls")
+vim.lsp.config("lua_language_server", require("lsp.lua_language_server"))
+vim.lsp.enable("lua_language_server")
 
 vim.lsp.config("gdscript", require("lsp.gdscript"))
 vim.lsp.enable("gdscript")
