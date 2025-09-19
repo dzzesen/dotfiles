58e763dcef4c83dc161ac5b937b5d8be398b1838
Dzenis Kopeikin <kopeykindzzesen@gmail.com>
2025-09-20 00:28:18 +0400

    replace pylsp to basepyright in nvim config

--- configs/nvim/lua/lsp/init.lua
+++ configs/nvim/lua/lsp/init.lua
@@ -1,17 +1,8 @@
-local registry = require("mason-registry")
-
-local pylsp_pkg = registry.get_package("python-lsp-server")
-if not pylsp_pkg:is_installed() then
-	pylsp_pkg:install()
-	local pip = vim.fn.expand("~/.local/share/nvim/mason/packages/python-lsp-server/venv/bin/pip")
-	vim.fn.system({ pip, "install", "pylsp-mypy" })
-end
+vim.lsp.config("basepyright", require("lsp.basepyright"))
+vim.lsp.enable("basepyright")
 
 vim.lsp.config("ruff", require("lsp.ruff"))
 vim.lsp.enable("ruff")
-
-vim.lsp.config("pylsp", require("lsp.pylsp"))
-vim.lsp.enable("pylsp")
 
 vim.lsp.config("lua_language_server", require("lsp.lua_language_server"))
 vim.lsp.enable("lua_language_server")
