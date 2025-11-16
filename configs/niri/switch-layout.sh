3da2d0ed2ed8a12fad4af43de876cc051870bc43
Dzenis Kopeikin <kopeykindzzesen@gmail.com>
2025-11-16 10:46:15 +0400

    add serbian layout

--- configs/niri/switch-layout.sh
+++ configs/niri/switch-layout.sh
@@ -4,8 +4,10 @@ out=$(niri msg keyboard-layouts)
 # Keyboard layouts:
 #  * 0 English (US)
 #    1 Russian
-#    2 Finnish
-#    3 Polish
+#    2 Serbian
+#    3 Serbian (cyrl)
+#    4 Finnish
+#    5 Polish
 
 if echo "$out" | grep -q '^\s*\* 0 '; then
     niri msg action switch-layout 1
