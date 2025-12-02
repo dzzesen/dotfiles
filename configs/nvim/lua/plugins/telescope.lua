7e866886fc5271ac28687fe2626eb040ae5275b6
Dzenis Kopeikin <kopeykindzzesen@gmail.com>
2025-12-02 20:23:37 +0530

    make telescope full screen

--- configs/nvim/lua/plugins/telescope.lua
+++ configs/nvim/lua/plugins/telescope.lua
@@ -2,4 +2,13 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		defaults = {
+			layout_config = {
+				height = 1000,
+				width = 1000,
+        preview_width = 0.7,
+			},
+		},
+	},
 }
