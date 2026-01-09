381ce6b310aa4550b8c2981611542ceefd9f7cd2
Dzenis Kopeikin <kopeykindzzesen@gmail.com>
2026-01-09 19:08:49 +0700

    shorten treesitter context

--- configs/nvim/lua/plugins/treesitter-context.lua
+++ configs/nvim/lua/plugins/treesitter-context.lua
@@ -2,5 +2,9 @@ return {
 	"nvim-treesitter/nvim-treesitter-context",
 	opts = {
     line_numbers = true,
+    max_lines = 3,
+    min_window_height = 20,
+    multiline_threshold = 1,
+    zindex = 20,
   },
 }
