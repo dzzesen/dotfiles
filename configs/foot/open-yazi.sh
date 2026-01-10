994d218dd180186e984f594255c143c4f8aedf56
Dzenis Kopeikin <kopeykindzzesen@gmail.com>
2026-01-10 13:20:00 +0700

    remove yazi

--- configs/foot/open-yazi.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-exec foot --app-id=yazi-float --working-directory="$PWD" --log-level=error yazi
--- configs/yazi/init.lua
+++
@@ -1,3 +0,0 @@
-function Linemode:file_info()
-	return string.format("%s  %6s  %s", self:permissions(), self:size(), self:mtime())
-end
