8fc05615716d81f04f80a805f92751025dc1210d
Dzenis Kopeikin <kopeykindzzesen@gmail.com>
2026-01-23 20:07:43 +0700

    use fish shell

---
+++ configs/fish/config.fish
@@ -0,0 +1,26 @@
+starship init fish | source
+
+set -U fish_greeting
+
+# start the ssh-agent service
+# systemctl --user enable --now ssh-agent
+set -Ux SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+if test -S $SSH_AUTH_SOCK
+    ssh-add -l >/dev/null 2>&1;\
+    or ssh-add\
+    ~/.ssh/aeza_my\
+    ~/.ssh/birthdaybot.pem\
+    ~/.ssh/bitbucket_rt\
+    ~/.ssh/common-ssh-tunnel.pem\
+    ~/.ssh/slack-ssh-tunnel.pem\
+    ~/.ssh/timebot.pem\
+    ~/.ssh/uptimebot.pem
+end
+
+fish_hybrid_key_bindings
+
+bind -M insert \cn down-or-search
+bind -M insert \cp up-or-search
+
+alias j=joshuto
+
--- configs/nushell/env.nu
+++ configs/nushell/env.nu
@@ -1,6 +1,6 @@
 do --env {
     let ssh_agent_file = (
-        $nu.temp-path | path join $"ssh-agent-(whoami).nuon"
+        $nu.temp-dir | path join $"ssh-agent-(whoami).nuon"
     )
 
     if ($ssh_agent_file | path exists) {
